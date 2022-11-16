d849b90a172c2332ad087262dc76bac3fb48826b
anggaagustiratelkom <anggaagustira@gmail.com>
2022-11-17 02:27:58 +0700

    Deploy react app

---
+++ precache-manifest.81bdf021ef9172a406dc043a18e361f2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "dc6b45d7a7ea21d94380",
+    "url": "/anggaagustiratelkom/react_web.git/static/css/main.30c5af2d.chunk.css"
+  },
+  {
+    "revision": "dc6b45d7a7ea21d94380",
+    "url": "/anggaagustiratelkom/react_web.git/static/js/main.dc6b45d7.chunk.js"
+  },
+  {
+    "revision": "95ea83b6c7492a61ef7b",
+    "url": "/anggaagustiratelkom/react_web.git/static/js/1.95ea83b6.chunk.js"
+  },
+  {
+    "revision": "fe9e49dcc9c458a203f8",
+    "url": "/anggaagustiratelkom/react_web.git/static/js/2.fe9e49dc.chunk.js"
+  },
+  {
+    "revision": "4006470aba7facbd2777",
+    "url": "/anggaagustiratelkom/react_web.git/static/js/runtime~main.4006470a.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/anggaagustiratelkom/react_web.git/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "260fb516c0303e9f5b7c5cf57f07628a",
+    "url": "/anggaagustiratelkom/react_web.git/index.html"
+  }
+];
